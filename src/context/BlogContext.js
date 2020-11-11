61ae229d596be95f7bf974a0f55014a63c251177
Veselin <vesselin60@gmail.com>
2020-11-11 12:12:54 +0200

    168. Editing Posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -41,7 +41,9 @@ const deleteBlogPost = (dispatch) => {
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+
     dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
     callback();
   };
